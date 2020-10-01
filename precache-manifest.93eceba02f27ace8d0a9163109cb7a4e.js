de53f0e961c83485fd48ccf5c6e5d03a7b995589
hushun1214 <2375353463@qq.com>
2020-10-01 11:50:22 +0800

    deploy

---
+++ precache-manifest.93eceba02f27ace8d0a9163109cb7a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9993cbf77229025a42aeecc37c2280",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "761a8df129e8190f8049",
+    "url": "/morney-website/static/css/main.5d423571.chunk.css"
+  },
+  {
+    "revision": "407167111e26f3c19772",
+    "url": "/morney-website/static/js/2.51e0a3ca.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/morney-website/static/js/2.51e0a3ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761a8df129e8190f8049",
+    "url": "/morney-website/static/js/main.b86207d7.chunk.js"
+  },
+  {
+    "revision": "c0539b70746be4102464",
+    "url": "/morney-website/static/js/runtime-main.8cd224f6.js"
+  }
+]);
